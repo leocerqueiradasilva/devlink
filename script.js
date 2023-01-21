36206bdc881a91ea116f2338d9efc7dd8dc04072
Leonardo Cerqueira da Silva <leocs.info@gmail.com>
2023-01-21 16:11:43 -0300

    devlink projeto

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 function toggleMode() {
     const html = document.documentElement;
-
     // if(html.classList.contains('light')){
     //     html.classList.remove('light')
     // }else{
     //     html.classList.add('light')
     // }
-
     html.classList.toggle('light'); /* Substitui todo o if descrito acima */
 
     //pergar a tag img
@@ -19,3 +17,4 @@ function toggleMode() {
         img.setAttribute('src', './assets/leocs_w.png')
     }
 }
+
